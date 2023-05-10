90fee88b0857cb0b1c546894e94d750d4f654da6
Elan Tuan <tuan.elan@gmail.com>
2023-05-09 21:28:26 -0300

    incluir  bootstrap e armazenamento em txt

--- cadastro.php
+++ cadastro.php
@@ -1,46 +1,28 @@
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Clientes</title>    
-</head>
-<body> 
-    <?php
-
-    $cliente [] = $_POST;
-    print_r($cliente);
-    
-   /* $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $data = $_POST['data'];
-    
-    $cliente = [
-    'name' => $nome,
-    'mail' => $email,
-    'date' => $data
-    ];
-    
+<?php
 
 
-  /* foreach ($cliente as $cadastroCliente) {
-    $cadastroCliente = [
+//Criamos uma função que recebe um texto como parâmetro.
+function gravar($cliente){
+	//Variável arquivo armazena o nome e extensão do arquivo.
+	$arquivo = "meu_arquivo.txt";
+	
+	//Variável $fp armazena a conexão com o arquivo e o tipo de ação.
+	$fp = fopen($arquivo, "a+");
 
-        'cliente'=> [
-            'name' => $nome,
-            'mail' => $email,
-            'date' => $data
-        ]
+	//Escreve no arquivo aberto.
+	fwrite($fp, $cliente);
+	
+	//Fecha o arquivo.
+	fclose($fp);
+}
 
+if ($_POST != null){
 
-    ];
-   };
-    */
- 
+$cliente[] = $_POST;
+print_r($cliente);
+gravar($cliente);
 
+}
+?>
 
-    ?>    
-</body>
-</html>
