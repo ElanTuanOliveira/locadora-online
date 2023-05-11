037f85ec0293d9205d1d54426a5d6ac17789f1e8
Elan Tuan <tuan.elan@gmail.com>
2023-05-10 23:09:12 -0300

    Criação da lista de clientes

---
+++ lista.php
@@ -0,0 +1,32 @@
+<?php
+
+
+//Criamos uma função que irá retornar o conteúdo do arquivo.
+function ler(): array {
+	//Variável arquivo armazena o nome e extensão do arquivo.
+	$arquivo = "cadastroclientes.txt";
+	
+	//Variável $fp armazena a conexão com o arquivo e o tipo de ação.
+	$fp = fopen($arquivo, "r");
+
+	//Lê o conteúdo do arquivo aberto.
+
+    $conteudo[] = fread($fp, filesize($arquivo));
+    
+    	//Fecha o arquivo.
+        fclose($fp);
+       
+
+
+        //retorna o conteúdo.
+        return $conteudo;
+}
+
+echo ler();
+
+
+
+
+
+
+?>
